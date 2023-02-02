a3e11f981c0874e67b3a0c469e61f3390a46f6f2
sqxy17 <115132354+sqxy17@users.noreply.github.com>
2023-02-02 23:30:15 +0800

    Add files via upload

---
+++ 验证密码小程序.c
@@ -0,0 +1,23 @@
+//#include<stdio.h>
+//#include<string.h>
+//int main(void)
+//{
+//    char password[]={"sqxyllk"};
+//    int n;
+//    char input[20]={0};
+//    do {
+//        printf("请输入密码");
+//        scanf("%s",input);
+//
+//        n=strcmp(input,password);
+//        if(n!=0)
+//        {
+//            printf("密码错误\n");
+//        }
+//        else
+//        {
+//            printf("密码正确");
+//        }
+//    }while(n!=0);
+//    return 0;
+//}
